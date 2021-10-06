b5ab4af46a431cdf287a6575a37bdd530a5af68c
inyestar <yuinye@gmail.com>
2021-10-06 00:04:39 -0400

    os 구분 script 추가

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,11 @@
 FROM centos:centos7.9.2009
-ARG PASS
+RUN yum -y install openssh-clients sshpass
 COPY ./install-collectd.sh /home/install.sh
 COPY ./collectd.conf /home/collectd.conf
-RUN yum -y install openssh-clients sshpass
+ARG USER
+ARG PASS
+ARG OS
+ENV USER=$USER
 ENV PASS=$PASS
+ENV OS=$OS
 CMD ["bash", "/home/install.sh"]
--- install-collectd.sh
+++ install-collectd.sh
@@ -2,7 +2,28 @@
 
 mkdir ~/.ssh
 ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa
-sshpass -p ${PASS} scp -o StrictHostKeyChecking=no ~/.ssh/id_rsa.pub root@127.0.0.1:~/.ssh/authorized_keys
-ssh root@127.0.0.1 'yum -y install collectd collectd-smart'
-sshpass -p ${PASS} scp -o StrictHostKeyChecking=no /home/collectd.conf root@127.0.0.1:/etc/collectd.conf
-ssh root@127.0.0.1 'systemctl enable collectd; systemctl start collectd'
+mkdir /home/.ssh
+cp -ra ~/.ssh/id_rsa.pub /home/.ssh/
+mv /home/.ssh/id_rsa.pub /home/.ssh/authorized_keys
+sshpass -p ${PASS} scp -o StrictHostKeyChecking=no -r /home/.ssh/ ${USER}@127.0.0.1:~/
+
+if [ "${OS}" == "centos" ]
+then
+    ssh ${USER}@127.0.0.1 'echo '${PASS}' | sudo -S yum -y install collectd collectd-smart'
+    sshpass -p ${PASS} scp -o StrictHostKeyChecking=no /home/collectd.conf ${USER}@127.0.0.1:~/collectd.conf
+    ssh ${USER}@127.0.0.1 'echo '${PASS}' | sudo -S mv -f ~/collectd.conf /etc/collectd.conf'
+    ssh ${USER}@127.0.0.1 'echo '${PASS}' | sudo -S chown root:root /etc/collectd.conf'
+elif [ "${OS}" == "ubuntu" ]
+then
+    ssh ${USER}@127.0.0.1 'echo '${PASS}' | sudo -S apt-get -y install collectd libatasmart-dev'
+    sshpass -p ${PASS} scp -o StrictHostKeyChecking=no /home/collectd.conf ${USER}@127.0.0.1:~/collectd.conf
+    ssh ${USER}@127.0.0.1 'echo '${PASS}' | sudo -S mv -f ~/collectd.conf /etc/collectd/collectd.conf'
+    ssh ${USER}@127.0.0.1 'echo '${PASS}' | sudo -S chown root:root /etc/collectd/collectd.conf'
+else
+    echo "No supoorted os found : OS="${OS}
+    exit 1
+fi
+
+ssh ${USER}@127.0.0.1 'echo '${PASS}' | sudo -S systemctl enable collectd'
+ssh ${USER}@127.0.0.1 'echo '${PASS}' | sudo -S systemctl start collectd'
+ssh ${USER}@127.0.0.1 'rm -rf ~/.ssh/authorized_keys'
